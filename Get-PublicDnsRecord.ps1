e512123f977dd6f975b52fee49798fbdade125b9
edemilliere <emmanuel.demilliere@hotmail.fr>
2017-04-16 10:18:44 +0200

    Add DynamicParam for Node choice

--- Get-PublicDnsRecord.ps1
+++ Get-PublicDnsRecord.ps1
@@ -18,34 +18,61 @@ Function Get-PublicDnsRecord{
     .LINK
     #>
     Param(
-        [Parameter(Mandatory=$true,Position=1)]
+        [Parameter(Mandatory=$true,
+            HelpMessage='DNS domain name to query.',
+            Position=1)]
         [String]$DomainName,
 
-        [Parameter(Mandatory=$true,Position=2)]
+        [Parameter(Mandatory=$true,
+            HelpMessage='DNS record type to query.',
+            Position=2)]
         [ValidateSet('A','AAAA','CERT','CNAME','DHCIP','DLV','DNAME','DNSKEY','DS','HINFO','HIP','IPSECKEY','KX','LOC','MX','NAPTR','NS','NSEC','NSEC3','NSEC3PARAM','OPT','PTR','RRSIG','SOA','SPF','SRV','SSHFP','TA','TALINK','TLSA','TXT')]
         [String[]]$DnsRecordType
     )
-    Begin{}
+
+    DynamicParam{        
+        $AttribColl = New-Object -TypeName System.Collections.ObjectModel.Collection[System.Attribute]
+	    $ParamAttrib = New-Object -TypeName System.Management.Automation.ParameterAttribute
+	    $ParamAttrib.Mandatory = $false
+	    $ParamAttrib.ParameterSetName = '__AllParameterSets'
+	    $ParamAttrib.ValueFromPipeline = $false
+	    $ParamAttrib.ValueFromPipelineByPropertyName = $false
+	    $AttribColl.Add($ParamAttrib)
+	    $AttribColl.Add((New-Object -TypeName System.Management.Automation.ValidateSetAttribute -ArgumentList ((Invoke-WebRequest -Uri 'http://www.dns-lg.com/nodes.json'| Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty nodes | Select-Object -ExpandProperty name))))
+	    $RuntimeParam = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameter -ArgumentList ('Node', [string], $AttribColl)
+	    $RuntimeParamDic = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameterDictionary
+	    $RuntimeParamDic.Add('Node', $RuntimeParam)
+	    $RuntimeParamDic
+    }
+
+    Begin{
+        $PsBoundParameters.GetEnumerator() | ForEach-Object -Process { New-Variable -Name $_.Key -Value $_.Value -ErrorAction 'SilentlyContinue' }
+        if(!$Node){$Node = Invoke-WebRequest -Uri 'http://www.dns-lg.com/nodes.json'| Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty nodes | Select-Object -ExpandProperty name | Get-Random}
+    }
     Process{
         ForEach($Record in $DnsRecordType){
             Try{
-                $WebUrl = 'http://www.dns-lg.com/opendns1/{0}/{1}' -f $DomainName,$Record
+                $WebUrl = 'http://www.dns-lg.com/{0}/{1}/{2}' -f $Node,$DomainName,$Record
                 
-                $WebData = Invoke-WebRequest $WebUrl -ErrorAction Stop | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty answer
+                Write-Verbose -Message "Constructed URL for query is $WebUrl."
+
+                $WebData = Invoke-WebRequest -Uri $WebUrl -ErrorAction Stop | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty answer
                 $WebData | % {
                      New-Object -TypeName PSObject -Property @{
-                        'Name'      = $_.name
-                        'Type'      = $_.type
-                        'Target'    = $_.rdata
+                        'Name'   = $_.name
+                        'Type'   = $_.type
+                        'Target' = $_.rdata
+                        'Node'   = $Node
                     }
                 }
             }
             catch{
                 Write-Warning -Message $_
                 New-Object -TypeName PSObject -Property @{
-                    'Name'      = $DomainName
-                    'Type'      = $Record
-                    'Target'    = ($_[0].ErrorDetails.Message -split '"')[-2]
+                    'Name'   = $DomainName
+                    'Type'   = $Record
+                    'Target' = ($_[0].ErrorDetails.Message -split '"')[-2]
+                    'Node'   = $Node
                 }
             }
         }
