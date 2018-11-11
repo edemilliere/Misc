176faff3a6d8c98dd07442e3b6850f88c7f52c80
edemilliere <emmanuel.demilliere@hotmail.fr>
2018-11-11 15:31:28 +0100

    Update Get-WifiPassword.ps1
    v1

---
+++ Get-WifiPassword.ps1
@@ -0,0 +1,11 @@
+﻿Function Get-WifiPassword {
+    netsh wlan show profile | Select-Object -Skip 3| Where-Object -FilterScript {($_ -like '*:*')} | ForEach-Object -Process {
+        $NetworkName = $_.Split(':')[-1].trim()
+        $PasswordDetection = $(netsh wlan show profile name =$NetworkName key=clear) | Where-Object -FilterScript {($_ -like '*contenu de la clé*') -or ($_ -like '*key content*')}
+
+        New-Object -TypeName PSObject -Property @{
+            NetworkName = $NetworkName
+            Password = if($PasswordDetection){$PasswordDetection.Split(':')[-1].Trim()}else{'Unknown'}
+        } -ErrorAction SilentlyContinue
+    } 
+}
