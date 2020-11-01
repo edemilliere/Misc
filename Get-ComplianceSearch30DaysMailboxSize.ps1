d9213cce9220fa2ccfbfb6e4dbc589f67c3b33cd
edemilliere <emmanuel.demilliere@hotmail.fr>
2020-11-01 14:10:54 +0100

    New 30 days of emails scripts
    Added 2 scripts to calculate the size of 30 days of emails in Exchange mailboxes.

---
+++ Get-ComplianceSearch30DaysMailboxSize.ps1
@@ -0,0 +1,47 @@
+﻿#requires -modules ExchangeOnlineManagement
+
+#region Prereqs
+#Exchange Online Connection, can beinstalled with:
+#Install-Module -Name ExchangeOnlineManagement -Scope CurrentUser
+Connect-IPPSSession
+#endregion
+
+#region Init
+[Regex]$Regex = "Location:\s(?<Identity>\w+@\w+.\w+),\sItem\scount:\s(?<ItemCount>\w+),\sTotal\ssize:\s(?<TotalSize>\w+)"
+
+$MailboxesToSearchIn = 'junk@itfordummies.net','dumbo@itfordummies.net'
+#$MailboxesToSearchIn = Get-Content MailboxesList.txt -join ','
+
+$ComplianceSearchName = "30Days - $MailboxesToSearchIn"
+
+$StartDate = (Get-Date).AddDays(-30).ToString("MM-dd-yyyy")
+$EndDate = (Get-Date).ToString("MM-dd-yyyy")
+$ContentMatchQuery = "(sent=$StartDate..$EndDate)(received=$StartDate..$EndDate)"
+#endregion
+
+#Create compliance search
+New-ComplianceSearch -ExchangeLocation $MailboxesToSearchIn -ContentMatchQuery $ContentMatchQuery -Name $ComplianceSearchName
+
+#Trigger search
+Get-ComplianceSearch -Identity $ComplianceSearchName | Start-ComplianceSearch
+
+#Wait a few minutes to let it complete
+do{
+    Write-Host -NoNewline '.'
+    Start-Sleep -Seconds 10
+}
+while((Get-ComplianceSearch -Identity $ComplianceSearchName).status -ne 'Completed')
+
+#Get the result
+$SuccessResults = (Get-ComplianceSearch -Identity $ComplianceSearchName | Select-Object -ExpandProperty SuccessResults) #-replace '\r\n'
+$RegexMatchingResult = $Regex.Matches($SuccessResults)
+$RegexMatchingResult | ForEach-Object -Process {
+    [PSCustomObject]@{
+        Identity = $_.Groups['Identity'].Value
+        ItemCount = $_.Groups['ItemCount'].Value        
+        TotalSizeinMB = $_.Groups['TotalSize'].Value/1MB -as [int]
+    }
+} | Export-Csv -NoTypeInformation -Delimiter ';' 30DaysOfEmailCalculatedFromComplianceSearch.csv
+
+#Cleanup
+Get-ComplianceSearch -Identity $ComplianceSearchName | Remove-ComplianceSearch -Confirm:$false
---
+++ Get-EducatedGuess30DaysMailboxSize.ps1
@@ -0,0 +1,31 @@
+﻿#requires -modules ExchangeOnlineManagement
+
+#Can be one or the other
+$Mailboxes = 'junk@itfordummies.net','dumbo@itfordummies.net'
+#$Mailboxes = Get-Content MailboxesList.txt
+
+#Exchange Online Connection, can beinstalled with:
+#Install-Module -Name ExchangeOnlineManagement -Scope CurrentUser
+Connect-ExchangeOnline
+
+#Main loop
+$Output = foreach($Mailbox in $Mailboxes){
+    
+    $OldestItem = Get-MailboxFolderStatistics -Identity $Mailbox -IncludeOldestAndNewestItems | Select-Object -ExpandProperty OldestItemReceivedDate | Sort-Object | Select-Object -First 1
+    $TotalSizeInMb = ("$(Get-MailboxStatistics -Identity $Mailbox | Select-Object -ExpandProperty TotalItemSize | Select-Object -ExpandProperty Value)" -split '\(| ')[-2].Replace(',','')/1MB -as [int]
+    $AgeInDays = (Get-Date) - $OldestItem | Select-Object -ExpandProperty Days
+    $DailyaverageInMB = $TotalSizeInMb/$AgeInDays
+    
+    [PSCustomObject]@{
+        Identity = $Mailbox
+        OldestItem = $OldestItem
+        TotalSizeInMB = $TotalSizeInMb
+        AgeInDays = $AgeInDays
+        DailyaverageInMB = '{0:N2}' -f $DailyaverageInMB
+        '30Daysaverage' = ($DailyaverageInMB*30) -as [int]
+    }
+    
+    $OldestItem = $TotalSizeInMb = $AgeInDays = $DailyaverageInMB = $null
+}
+
+$Output | Export-Csv -NoTypeInformation -Delimiter ';' 30DaysOfEmailCalculatedFromTotalSizeAndLifeTimeAverage.csv
